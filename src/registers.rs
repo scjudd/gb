0eb42d7a25941ef744d9044bbbd3054d2a825f2d
Spencer Judd <spencercjudd@gmail.com>
2019-08-26 18:01:14 -0400

    Set initial CPU state as if bootloader had run

--- src/registers.rs
+++ src/registers.rs
@@ -96,16 +96,16 @@ pub struct Registers {
 impl Registers {
     pub fn new() -> Registers {
         Registers {
-            a: 0,
-            f: 0,
-            b: 0,
-            c: 0,
-            d: 0,
-            e: 0,
-            h: 0,
-            l: 0,
-            sp: 0,
-            pc: 0x100,
+            a: 0x01,
+            f: 0xb0,
+            b: 0x00,
+            c: 0x13,
+            d: 0x00,
+            e: 0xd8,
+            h: 0x01,
+            l: 0x4d,
+            sp: 0xfffe,
+            pc: 0x0100,
             ime: true,
         }
     }
