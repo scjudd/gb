81a9098565ca90e6d863b907837255bdf8ae0d68
Spencer Judd <spencercjudd@gmail.com>
2019-08-26 13:30:23 -0400

    Implement Vblank interrupt handling

--- src/main.rs
+++ src/main.rs
@@ -25,13 +25,34 @@ fn main() {
     }
 }
 
-fn next(reg: &mut Registers, bus: &mut AddressBus) {
+fn next(mut reg: &mut Registers, mut bus: &mut AddressBus) {
     let pc = reg.get_pc();
     let opcode = bus.read_8bit(pc);
     let inst = OPCODES[opcode as usize];
     inst.execute(reg, bus);
     println!("{:04x}: {:02x} {}", pc, opcode, inst.mnemonic(pc, bus));
 
+    if reg.get_ime() {
+        let reg_if = bus.read_8bit(0xff0f);
+        let reg_ie = bus.read_8bit(0xffff);
+        let fired = reg_if & reg_ie;
+
+        // Vblank
+        if fired & 0x01 == 0x01 {
+            bus.write_8bit(0xff0f, reg_if & !0x01); // Don't fire again
+            handle_interrupt(0x40, &mut reg, &mut bus);
+        }
+    }
+
     // TODO: use num_cycles and measure our current execution speed to throttle our execution speed
     // to 4.20MHz for DMG, 8.4MHz for CGB in double-speed mode.
+}
+
+fn handle_interrupt(addr: u16, reg: &mut Registers, bus: &mut AddressBus) {
+    reg.set_ime(false);
+    reg.set_sp_offset(-2);
+    bus.write_16bit(reg.get_sp(), reg.get_pc());
+    reg.set_pc(addr);
+
+    // This costs 12 cycles on top of however many the actual interrupt handler takes
 }
