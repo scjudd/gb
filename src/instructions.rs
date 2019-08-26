a0765b3a1f13fd3fa347c53bc3dc9c4771d186f5
Spencer Judd <spencercjudd@gmail.com>
2019-08-26 17:31:08 -0400

    Fix warnings for Load8Bit

--- src/instructions.rs
+++ src/instructions.rs
@@ -57,13 +57,13 @@ pub struct Load8Bit {
 }
 
 impl Instruction for Load8Bit {
-    fn execute(&self, reg: &mut Registers, bus: &mut AddressBus) {
+    fn execute(&self, reg: &mut Registers, _bus: &mut AddressBus) {
         reg.inc_pc(2);
         let val = reg.get_8bit(self.src);
         reg.set_8bit(self.dst, val);
     }
 
-    fn mnemonic(&self, addr: u16, bus: &AddressBus) -> String {
+    fn mnemonic(&self, _addr: u16, _bus: &AddressBus) -> String {
         format!("LD {},{}", self.dst, self.src)
     }
 }
